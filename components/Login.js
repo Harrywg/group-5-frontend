4c84dda3392151b5fbd9857d1b83428e857ec19a
jay <jay241093@gmail.com>
2023-09-20 12:14:43 +0100

    Added a bottom sheet which will display our places data

--- components/Login.js
+++ components/Login.js
@@ -19,7 +19,6 @@ export default function Login({ isAuthenticated, setIsAuthenticated }) {
 
   const navigation = useNavigation();
 
-  console.log(navigation);
 
   const handleLogin = () => {
     setLoading(true);
