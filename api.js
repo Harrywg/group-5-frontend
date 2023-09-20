b8663fe46ad22d6d5642083b9acde13f062a64a9
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-20 11:56:25 +0100

    generate leaderboards statically from api

---
+++ api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://group-5-backend-ztnc.onrender.com/api",
+});
+
+export const getUsers = () => {
+  console.log("invoke");
+  return api.get("/users").then(({ data }) => data);
+};
--- components/LeaderBoard.js
+++ components/LeaderBoard.js
@@ -1,12 +1,31 @@
 import { useNavigation } from "@react-navigation/native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View, Button } from "react-native";
 import mainStyles from "../styles/mainStyles";
+import { getUsers } from "../api";
 
 export default function LeaderBoard({ navigation }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((data) => {
+      console.log(data);
+      setUsers(data);
+    });
+  }, []);
+
+  useEffect(() => console.log({ users }), [users]);
+
   return (
     <View style={mainStyles.container}>
       <Text style={mainStyles.text}>LeaderBoard</Text>
+      {users.map((user, i) => {
+        return (
+          <Text style={{ color: "white" }}>
+            {++i}. {user.username}
+          </Text>
+        );
+      })}
     </View>
   );
 }
