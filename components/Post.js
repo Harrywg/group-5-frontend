45ce4eaedf0a5c220d5184b04f93658aa9b42351
Lewis Lawler <lewislawler98@gmail.com>
2023-09-21 15:54:44 +0100

    adds conditional rendering when picture is submitted to show postPlace form with input fields and picture just taken

--- components/Post.js
+++ components/Post.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
 import Constants from "expo-constants";
 import { Camera, CameraType } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
 import { MaterialIcons, Entypo } from "@expo/vector-icons";
+import mainStyles from "../styles/mainStyles";
 
 export default function PostPlace() {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const [submittedImage, setSubmittedImage] = useState(false);
   const cameraRef = useRef(null);
+  const [placeName, setPlaceName] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -32,12 +42,10 @@ export default function PostPlace() {
     }
   };
 
-  const savePicture = async () => {
+  const submitPicture = async () => {
     if (image) {
       try {
-        const asset = await MediaLibrary.createAssetAsync(image);
-        alert("Picture saved! 🎉");
-        setImage(null);
+        setSubmittedImage(true);
         console.log("saved successfully");
       } catch (error) {
         console.log(error);
@@ -45,74 +53,110 @@ export default function PostPlace() {
     }
   };
 
+  const handlePlaceSubmit = async () => {
+    try {
+      alert("Place submitted! 🎉");
+
+      setSubmittedImage(false);
+      setImage(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (hasCameraPermission === false) {
     return <Text>No access to camera</Text>;
   }
 
-  return (
-    <View style={styles.container}>
-      {!image ? (
-        <Camera
-          style={styles.camera}
-          type={type}
-          ref={cameraRef}
-          flashMode={flash}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              paddingHorizontal: 30,
-            }}
+  if (!submittedImage) {
+    return (
+      <View style={styles.container}>
+        {!image ? (
+          <Camera
+            style={styles.camera}
+            type={type}
+            ref={cameraRef}
+            flashMode={flash}
           >
-            <Button
-              title=""
-              icon="retweet"
-              onPress={() => {
-                setType(
-                  type === CameraType.back ? CameraType.front : CameraType.back
-                );
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                paddingHorizontal: 30,
               }}
-            />
-            <Button
-              onPress={() =>
-                setFlash(
-                  flash === Camera.Constants.FlashMode.off
-                    ? Camera.Constants.FlashMode.on
-                    : Camera.Constants.FlashMode.off
-                )
-              }
-              icon="flash"
-              color={flash === Camera.Constants.FlashMode.off ? "gray" : "#fff"}
-            />
-          </View>
-        </Camera>
-      ) : (
-        <Image source={{ uri: image }} style={styles.camera} />
-      )}
-
-      <View style={styles.controls}>
-        {image ? (
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              paddingHorizontal: 50,
-            }}
-          >
-            <Button
-              title="Re-take"
-              onPress={() => setImage(null)}
-              icon="retweet"
-            />
-            <Button title="Save" onPress={savePicture} icon="check" />
-          </View>
+            >
+              <Button
+                title=""
+                icon="retweet"
+                onPress={() => {
+                  setType(
+                    type === CameraType.back
+                      ? CameraType.front
+                      : CameraType.back
+                  );
+                }}
+              />
+              <Button
+                onPress={() =>
+                  setFlash(
+                    flash === Camera.Constants.FlashMode.off
+                      ? Camera.Constants.FlashMode.on
+                      : Camera.Constants.FlashMode.off
+                  )
+                }
+                icon="flash"
+                color={
+                  flash === Camera.Constants.FlashMode.off ? "gray" : "#fff"
+                }
+              />
+            </View>
+          </Camera>
         ) : (
-          <Button title="Take a picture" onPress={takePicture} icon="camera" />
+          <Image source={{ uri: image }} style={styles.camera} />
         )}
+
+        <View style={styles.controls}>
+          {image ? (
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                paddingHorizontal: 50,
+              }}
+            >
+              <Button
+                title="Re-take"
+                onPress={() => setImage(null)}
+                icon="retweet"
+              />
+              <Button title="Submit" onPress={submitPicture} icon="check" />
+            </View>
+          ) : (
+            <Button
+              title="Take a picture"
+              onPress={takePicture}
+              icon="camera"
+            />
+          )}
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else {
+    console.log(image, "image");
+    return (
+      <View style={mainStyles.container}>
+        <Text style={mainStyles.text}>Post your place</Text>
+        <TextInput
+          placeholder="Place name"
+          value={placeName}
+          onChangeText={(text) => setPlaceName(text)}
+          style={styles.input}
+        />
+        <Image source={{ uri: image }} style={styles.image} />
+        <Button title="Post Place" onPress={handlePlaceSubmit} />
+      </View>
+    );
+  }
 }
 
 function Button({ title, onPress, icon, color }) {
@@ -154,5 +198,19 @@ const styles = StyleSheet.create({
   },
   topControls: {
     flex: 1,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  image: {
+    width: 200, 
+    height: 200,
+    resizeMode: "cover", 
+    borderRadius: 10, 
+    marginTop: 10, 
   },
 });
