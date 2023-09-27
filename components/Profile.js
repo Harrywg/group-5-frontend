735150f59782a08640c66d0ae1b786e53f6d8c01
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-27 16:21:07 +0100

    style profile page with medals

--- components/Profile.js
+++ components/Profile.js
@@ -6,10 +6,15 @@ import { StyleSheet } from "react-native";
 import { useAuth } from "../context";
 import { deleteUser, updateUsers, getUsersByUsername } from "../api";
 
+const medals = {
+  bronze: require("./img/bronze-medal.png"),
+  silver: require("./img/silver-medal.png"),
+  gold: require("./img/gold-medal.png"),
+};
+
 export default function Profile({ navigation }) {
   const { user, login } = useAuth();
   console.log(user, "USER");
-
   const handleLogout = () => {
     navigation.navigate("Login");
   };
@@ -30,35 +35,52 @@ export default function Profile({ navigation }) {
       }
     }
   };
-
-  const handleEdit = async () => {};
-
+  function calcPoints(medals) {
+    const { gold, silver, bronze } = medals;
+    return gold * 100 + silver * 50 + bronze * 25;
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
-        <Image
-          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-          style={styles.avatar}
-        />
+        <Image src={user.avatar_URL} style={styles.avatar} />
 
         <Text style={styles.username}>{user.username}</Text>
         <Text>
           Joined {new Date(user.createdAt).toLocaleString().split(",")[0]}
         </Text>
-        <Text>Gold: {user?.achievements?.gold}</Text>
-        <Text>Silver: {user?.achievements?.silver}</Text>
-        <Text>Bronze: {user?.achievements?.bronze}</Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold", color: "#3FC1C0" }}>
+          {calcPoints(user.achievements)} Points
+        </Text>
 
-        <TouchableOpacity style={styles.button} onPress={handleLogout}>
-          <Text style={styles.buttonText}>Edit Profile</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleLogout}>
-          <Text style={styles.buttonText}>Log Out</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleDelete}>
-          <Text style={styles.buttonText}>Delete Account</Text>
-        </TouchableOpacity>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            gap: 10,
+          }}
+        >
+          <View style={styles.medalWrap}>
+            <Image style={styles.medal} source={medals.gold} />
+            <Text style={styles.medalNumber}>{user?.achievements?.gold}</Text>
+          </View>
+          <View style={styles.medalWrap}>
+            <Image style={styles.medal} source={medals.silver} />
+            <Text style={styles.medalNumber}>{user?.achievements?.gold}</Text>
+          </View>
+          <View style={styles.medalWrap}>
+            <Image style={styles.medal} source={medals.bronze} />
+            <Text style={styles.medalNumber}>{user?.achievements?.gold}</Text>
+          </View>
+        </View>
+        <View style={styles.buttonWrap}>
+          <TouchableOpacity style={styles.button} onPress={handleLogout}>
+            <Text style={styles.buttonText}>Log Out</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteButton} onPress={handleDelete}>
+            <Text style={styles.buttonText}>Delete Account</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -74,6 +96,7 @@ const styles = StyleSheet.create({
     height: 150,
     width: 150,
     borderRadius: 75,
+    backgroundColor: "#3FC1C0",
   },
   profile: {
     flex: 1,
@@ -94,5 +117,36 @@ const styles = StyleSheet.create({
     borderRadius: 1000,
     padding: 10,
     paddingHorizontal: 20,
+    height: 41,
+  },
+  medal: {
+    height: 50,
+    width: 50,
+  },
+  medalWrap: {
+    flex: 1,
+    flexDirection: "column",
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  medalNumber: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  buttonWrap: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 10,
+    position: "absolute",
+    bottom: 20,
+  },
+  deleteButton: {
+    backgroundColor: "red",
+    borderRadius: 1000,
+    padding: 10,
+    paddingHorizontal: 20,
+    height: 41,
   },
 });
