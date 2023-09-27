a1a3aca166baba1c714c190fde7cee3cf7e4ecfe
jay <jay241093@gmail.com>
2023-09-27 14:01:21 +0100

    added refresh feature to leaderboard

--- components/LeaderBoard.js
+++ components/LeaderBoard.js
@@ -1,9 +1,8 @@
-import { useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { useEffect, useState, useCallback } from "react";
 import { Text, View, Image } from "react-native";
 import { getUsers } from "../api";
 import { StyleSheet } from "react-native";
-
 
 //Bronze: 25
 //Silver: 50
@@ -18,53 +17,61 @@ export default function LeaderBoard() {
     });
   }, []);
 
+  useFocusEffect(
+    useCallback(() => {
+      const fetchUsers = async () => {
+        try {
+          const data = await getUsers();
+          setUsers(data);
+        } catch (error) {
+          console.error("Error fetching users:", error);
+        }
+      };
+      fetchUsers();
+    }, [])
+  );
 
-
-  const usersWithPoints = [...users]
+  const usersWithPoints = [...users];
   usersWithPoints.map((user) => {
-  user.points=(user.achievements.bronze*25)+(user.achievements.silver*50)+(user.achievements.gold*100)
-  })
+    user.points =
+      user.achievements.bronze * 25 +
+      user.achievements.silver * 50 +
+      user.achievements.gold * 100;
+  });
 
-
-    
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>🏆    leaderboard    🏆</Text>
-      {usersWithPoints.sort((a,b)=>b.points-a.points).slice(0,10).map((user, i) => {
-        return (
-          <View key={user._id} style={styles.leaderBoard}>
-            <Text style={styles.number}>
-              {++i}
-            </Text>
-            <Image src={user.avatar_URL} style={styles.avatar}/>
-            <Text style={styles.name}>
-              {user.username}
-            </Text>
-            <Text style={styles.points} >
-              {user.points}
-            </Text>
-          </View>
-        );
-      })}
+      <Text style={styles.header}>🏆 Leaderboard 🏆</Text>
+      {usersWithPoints
+        .sort((a, b) => b.points - a.points)
+        .slice(0, 10)
+        .map((user, i) => {
+          return (
+            <View key={user._id} style={styles.leaderBoard}>
+              <Text style={styles.number}>{++i}</Text>
+              <Image src={user.avatar_URL} style={styles.avatar} />
+              <Text style={styles.name}>{user.username}</Text>
+              <Text style={styles.points}>{user.points}</Text>
+            </View>
+          );
+        })}
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     height: "100%",
     paddingTop: 75,
-    paddingBottom:10,
+    paddingBottom: 10,
     paddingHorizontal: 20,
-    gap: 5, 
-
+    gap: 5,
   },
   leaderBoard: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent:'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     borderWidth: 2,
     borderColor: "#3FC1C0",
     borderRadius: 15,
@@ -72,36 +79,33 @@ const styles = StyleSheet.create({
   avatar: {
     height: 45,
     width: 45,
-    backgroundColor: 'white',
-    marginLeft:40,
+    backgroundColor: "white",
+    marginLeft: 40,
     borderRadius: 50,
-    position:'absolute'
+    position: "absolute",
   },
   name: {
     fontSize: 20,
-   
   },
   number: {
     fontSize: 20,
     marginLeft: 10,
-    marginRight:50
-   
+    marginRight: 50,
   },
   points: {
     fontWeight: "bold",
     fontSize: 25,
     paddingRight: 15,
     color: "#3FC1C0",
-
   },
   header: {
     fontSize: 25,
-    textAlign: 'center',
+    textAlign: "center",
     marginRight: 10,
     fontWeight: "bold",
     padding: 5,
     borderWidth: 4,
     borderColor: "#CCDCDC",
-    borderRadius:15,
-  }
+    borderRadius: 15,
+  },
 });
