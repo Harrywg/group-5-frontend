57681769788936c0c750fae57e9d355d36c00bc3
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-28 09:27:14 +0100

    remove log in context

--- context.js
+++ context.js
@@ -10,12 +10,8 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const login = (userData) => {
-    setUser( userData );
+    setUser(userData);
   };
-
-  useEffect(() => {
-    console.log(user, 'CONTEXT')
-  }, [user])
 
   return (
     <AuthContext.Provider value={{ user, login }}>
